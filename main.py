93ba92fcbd1e0d190340d5e4c4218d8d1a826be8
Hyunuk Lee <as0266@naver.com>
2022-10-08 22:10:46 +0900

    add CORS

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 
 app = FastAPI()
+
+origins = [
+    "https://beaverhouse.github.io",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Item(BaseModel):
   memberNo: str
