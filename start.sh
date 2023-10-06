7ecad970bc5fedd41970c2c91c6ec90213f65aee
LHU <haulrest@naver.com>
2023-10-07 01:10:09 +0900

    python3.9 version specify 제거
    3.8로 계속 깔려서 버전명시 제거

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Ubuntu OS 기준
 sudo apt update
-sudo apt install python3.9 -y
+sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install gunicorn -y
 pip install -r requirements.txt
