13da27f319bed7428584a677e7de72906e34ad06
LHU <haulrest@naver.com>
2023-10-07 00:07:53 +0900

    add shell file

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3.9 -y
+sudo apt install python3-pip -y
+sudo apt install gunicorn -y
+pip install -r requirements.txt
+sudo gunicorn main:app --bind 0.0.0.0:80 --workers 4 --worker-class uvicorn.workers.UvicornWorker --daemon
